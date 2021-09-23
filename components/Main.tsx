ad808a4439ada2a0cd17a71fb4a88e21fbe70e43
mrmtsu <81202muramatsu@gmail.com>
2021-09-23 18:46:58 +0900

    💄 スタイルファイルの分割

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,13 +1,13 @@
 import { Headline } from "../components/Headline";
 import { Links } from "../components/Links";
-import styles from "../styles/Home.module.css";
+import classes from "../components/Main.module.css";
 import { HeadLineTitle } from "../types/type";
 
 export const Main = (props: HeadLineTitle) => {
   return (
-    <main className={styles.main}>
+    <main className={classes.main}>
       <Headline page={props.page}>
-        <code className={styles.code}>pages/{props.page}.js</code>
+        <code className={classes.code}>pages/{props.page}.js</code>
       </Headline>
       <Links />
     </main>
